89453836cfaff94a43d222d1f0d9658f08cea58d
Seryev <70902024+Seryev@users.noreply.github.com>
2023-04-19 21:12:41 -0600

    Creacion del archivo python3 que servira como API

---
+++ api.py
@@ -0,0 +1,13 @@
+'''
+Modulo para la API
+
+Autores: Seryev Oyser, Agergen sus nombres, Agregen sus nombres
+Fecha de creacion: 19 de abril del 2023
+Derechos resrevados: (c) 2023 EDSUBA. Todos los derechos reservados.
+Version: 0.1
+Dependecias: Python3 (version ?????),Flask, mariadb (version ?????)
+
+Descripcion: Lorem_impus
+'''
+from flask import Flask, jsonify 
+
