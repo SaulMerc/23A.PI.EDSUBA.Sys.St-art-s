33c09c8a19d5538351454cce12409c7bf5dd1901
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-05-16 14:05:00 -0600

    Actualización de requirements

--- app/database.py
+++ app/database.py
@@ -4,6 +4,6 @@ import mysql.connector
 database = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "root",
+    password = "Alg0R@ndo5",
     database = "db_starts"
 )
