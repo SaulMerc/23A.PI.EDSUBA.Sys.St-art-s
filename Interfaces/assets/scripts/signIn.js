3b9a62230005d240503b5b214fa4b950647c7461
Diego Hernández <111621211+DiegoHC7@users.noreply.github.com>
2023-05-20 16:37:09 -0600

    Interface de inicio de sesión

---
+++ Interfaces/assets/scripts/signIn.js
@@ -0,0 +1,18 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Comprueba si los campos de nombre de usuario y contraseña están vacíos
+    if (username == "" || password == "") {
+      alert("Por favor ingrese nombre de usuario y contraseña");
+      return false;
+    }
+    else {
+      // Lógica de inicio de sesión aquí
+      // Si los detalles de inicio de sesión son correctos, redirigir al usuario a la página principal
+      // De lo contrario, se mostrará un mensaje de error
+      alert("Inicio de sesión exitoso");
+      return true;
+    }
+  }
+  
