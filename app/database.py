f8cf53e79c0161f1b4fc6a9bea26741b07d5932d
HereIsWaldo <waldo.gomez4962@alumnos.udg.mx>
2023-05-20 20:19:10 -0600

    Prueba

--- app/database.py
+++ app/database.py
@@ -4,6 +4,6 @@ import mysql.connector
 database = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "Edsubapasswd07",
+    password = "Edsubapaswwd07",
     database = "db_starts_pruebas"
 )
